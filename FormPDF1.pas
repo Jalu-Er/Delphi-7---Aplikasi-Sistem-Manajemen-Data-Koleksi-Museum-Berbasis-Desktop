b1fd8319c5eebc8060bb1c54e30b37fe16debff7
JE <76509905+Jalu-Er@users.noreply.github.com>
2025-02-10 17:04:05 +0700

    Add files via upload

---
+++ FormPDF1.pas
@@ -0,0 +1,118 @@
+unit FormPDF1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, QuickRpt, utama1, QRCtrls, QRExport;
+
+type
+  TFormPDF = class(TForm)
+    QuickRepSeni: TQuickRep;
+    PageHeaderBand1: TQRBand;
+    ColumnHeaderBand1: TQRBand;
+    DetailBand1: TQRBand;
+    QRLabel1: TQRLabel;
+    QRShape2: TQRShape;
+    QRShape3: TQRShape;
+    QRShape4: TQRShape;
+    QRShape5: TQRShape;
+    QRShape6: TQRShape;
+    QRShape7: TQRShape;
+    QRShape8: TQRShape;
+    QRLabel2: TQRLabel;
+    QRShape1: TQRShape;
+    QRLabel3: TQRLabel;
+    QRLabel4: TQRLabel;
+    QRLabel5: TQRLabel;
+    QRLabel6: TQRLabel;
+    QRLabel7: TQRLabel;
+    QRLabel8: TQRLabel;
+    QRLabel9: TQRLabel;
+    QRShape9: TQRShape;
+    QRShape10: TQRShape;
+    QRShape11: TQRShape;
+    QRShape12: TQRShape;
+    QRShape13: TQRShape;
+    QRShape14: TQRShape;
+    QRShape15: TQRShape;
+    QRShape16: TQRShape;
+    QRDBText1: TQRDBText;
+    QRDBText2: TQRDBText;
+    QRDBText3: TQRDBText;
+    QRDBText4: TQRDBText;
+    QRDBText5: TQRDBText;
+    QRDBText6: TQRDBText;
+    QRDBText7: TQRDBText;
+    QRDBText8: TQRDBText;
+    QuickRepDonatur: TQuickRep;
+    QRBandDonatur: TQRBand;
+    QRLabel10: TQRLabel;
+    QRBand2: TQRBand;
+    QRShape17: TQRShape;
+    QRShape18: TQRShape;
+    QRShape19: TQRShape;
+    QRShape24: TQRShape;
+    QRLabel11: TQRLabel;
+    QRLabel12: TQRLabel;
+    QRLabel13: TQRLabel;
+    QRLabel14: TQRLabel;
+    QRBand3: TQRBand;
+    QRShape29: TQRShape;
+    QRShape30: TQRShape;
+    QRShape31: TQRShape;
+    QRShape32: TQRShape;
+    QRDBText9: TQRDBText;
+    QRDBText10: TQRDBText;
+    QRDBText11: TQRDBText;
+    QRDBText12: TQRDBText;
+    QuickRepSeniman: TQuickRep;
+    QRBand1: TQRBand;
+    QRLabel15: TQRLabel;
+    QRBand4: TQRBand;
+    QRShape20: TQRShape;
+    QRShape21: TQRShape;
+    QRShape23: TQRShape;
+    QRLabel16: TQRLabel;
+    QRLabel17: TQRLabel;
+    QRLabel18: TQRLabel;
+    QRBand5: TQRBand;
+    QRShape26: TQRShape;
+    QRShape27: TQRShape;
+    QRShape28: TQRShape;
+    QRDBText13: TQRDBText;
+    QRDBText14: TQRDBText;
+    QRDBText15: TQRDBText;
+    QuickRepKategori: TQuickRep;
+    QRBand6: TQRBand;
+    QRLabel19: TQRLabel;
+    QRBand7: TQRBand;
+    QRShape22: TQRShape;
+    QRShape25: TQRShape;
+    QRShape33: TQRShape;
+    QRLabel20: TQRLabel;
+    QRLabel21: TQRLabel;
+    QRLabel22: TQRLabel;
+    QRBand8: TQRBand;
+    QRShape34: TQRShape;
+    QRShape35: TQRShape;
+    QRShape36: TQRShape;
+    QRDBText16: TQRDBText;
+    QRDBText17: TQRDBText;
+    QRDBText18: TQRDBText;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormPDF: TFormPDF;
+
+implementation
+
+{$R *.dfm}
+
+
+
+end.
---
+++ foto1.pas
@@ -0,0 +1,25 @@
+unit foto1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls;
+
+type
+  TFoto = class(TForm)
+    PopupImage: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Foto: TFoto;
+
+implementation
+
+{$R *.dfm}
+
+end.
